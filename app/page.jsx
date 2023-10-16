ae28572cf99f202c9576cf8b19f8282f672551df
pravel <123407140+ElMatheus@users.noreply.github.com>
2023-10-16 14:45:17 -0300

    feat: diminuida na array

--- app/page.jsx
+++ app/page.jsx
@@ -21,17 +21,27 @@ export default function Home() {
     fetchData();
   }, []);
 
-  const filteredData = apiData.filter(item => item.type.value === 'outfit');
+  const filteredData = apiData.filter(item => item.type.value == 'outfit' && item.name !== 'TBD' && item.name !== 'null');
+
 
   console.log(filteredData);
   return (
     <div>
       {
-        filteredData.map(item => (
-            <div>
-              <h1>{item.type.value}</h1>
-            </div>
-        ))
+        filteredData.map((item, index) => {
+          if (index < 1725) {
+            return (
+              <div key={index}>
+                <p>{index}</p>
+                <h1>{item.name}</h1>
+                <img src={item.images.icon} width={256} height={256} />
+              </div>
+            );
+          } else {
+            console.log('erro');
+            return null; // ou outro elemento que você queira renderizar
+          }
+        })
       }
     </div>
   )
