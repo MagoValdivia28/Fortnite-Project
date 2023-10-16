371e2b485cc1cab4bfb01b90f1fa789dc2a1f7f4
felipe pedro <felipepedro.senai564@gmail.com>
2023-10-16 17:13:29 -0300

    loading de espera feito

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,11 @@
 "use client"
-
 import roupas from 'data/roupas';
-import { useEffect } from 'react';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import { Hearts } from 'react-loader-spinner';
 
 export default function Home() {
   const [apiData, setApiData] = useState([]);
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -19,48 +17,55 @@ export default function Home() {
     }
     fetchData();
   }, []);
-
-  const filteredData = apiData.filter(item => item.type.value == 'outfit' && item.name !== 'TBD' && item.name !== 'null' && item.introduction !== null);
-
-
-
+  
+  const filteredData = apiData.filter(item => item.type.value === 'outfit' && item.name !== 'TBD' && item.name !== 'null' && item.introduction !== null);
+  
   console.log(filteredData);
-
+  
   const formattedData = (date) => {
     const format = date.slice(0, 10);
     return format.split('-').reverse().join('/');
   }
-
+  
   const price = (item) => {
-    if (item.rarity.displayValue == 'Legendary') {
-      return "2000 v-bucks"//favor colocar o icon no lugar dessa escrita tenebrosa;
+    if (item.rarity.displayValue === 'Legendary') {
+      return "2000 v-bucks"; // Substitua essa string pelo ícone apropriado
     }
-   else if (item.rarity.displayValue == 'Rare') {
-      return "1200 v-bucks" //favor colocar o icon no lugar dessa escrita tenebrosa;
+    else if (item.rarity.displayValue === 'Rare') {
+      return "1200 v-bucks"; // Substitua essa string pelo ícone apropriado
     }
-   else if (item.rarity.displayValue == 'Uncommon') {
-      return "800 v-bucks" //favor colocar o icon no lugar dessa escrita tenebrosa;
+    else if (item.rarity.displayValue === 'Uncommon') {
+      return "800 v-bucks"; // Substitua essa string pelo ícone apropriado
     }
-   else if (item.rarity.displayValue == 'Common') {
-      return "500 v-bucks" //favor colocar o icon no lugar dessa escrita tenebrosa;
+    else if (item.rarity.displayValue === 'Common') {
+      return "500 v-bucks"; // Substitua essa string pelo ícone apropriado
     }
     else {
-      return "1500 v-bucks" //favor colocar o icon no lugar dessa escrita tenebrosa;
+      return "1500 v-bucks"; // Substitua essa string pelo ícone apropriado
     }
   }
+  
   return (
     <div>
-      {
-
-        // quando fazer o carregar é aqui que voce vai mexer blz?
-
-
+      {apiData.length == 0 ? (
+        <div>
+          <Hearts 
+            height={80}
+            width={80}
+            color="#4fa94d"
+            ariaLabel='hearts-loading'
+            wrapperStyle={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+            wrapperClass=''
+            visible={true}
+          />
+        </div>
+      ) : (
         filteredData.map((item, index) => {
           if (index < 1699) {
             return (
               <div key={index}>
                 <h1>{item.name}</h1>
-                <img src={item.images.icon} width={256} height={256} />
+                <img src={item.images.icon} width={256} height={256} alt={item.name} />
                 <p>{item.rarity.displayValue}</p>
                 <p>{item.introduction.text}</p>
                 <p>{formattedData(item.added)}</p>
@@ -69,7 +74,7 @@ export default function Home() {
             );
           }
         })
-      }
+      )}
     </div>
   )
 }
