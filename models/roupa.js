cf3ec3ab212c8f1c6251b89ac659f264c6417d9a
pravel <123407140+ElMatheus@users.noreply.github.com>
2023-10-16 10:53:11 -0300

    feat: class

--- models/roupa.js
+++ models/roupa.js
@@ -1,6 +1,20 @@
-class Roupa {
+export default class Roupa {
     constructor(nome,raridade,preco, capitulo, dataLancamento) {
         this.nome = nome;
-        
+        this.raridade = raridade;
+        this.preco = preco;
+        this.capitulo = capitulo;
+        this.dataLancamento = dataLancamento;
+        this.id = this.generateId();
+        this.imagem = this.generateImage();
+    }
+
+    generateId() {
+        return parseInt(Math.random() * 9999);
+    }
+
+    generateImage() {
+        return 'https://fortniteskins.net/wp-content/cache/thumb/91/9ce682852850991_666x630.webp'
     }
 }
+
