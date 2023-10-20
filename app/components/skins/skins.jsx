b40887991037381280a417efb1cadf6229e01b26
Pedro <pedrosouza.senai564@gmail.com>
2023-10-20 17:09:28 -0300

    feat: sobrenos feito

--- app/components/skins/skins.jsx
+++ app/components/skins/skins.jsx
@@ -1,19 +1,19 @@
 import styles from "./skins.module.css"
 import Price from '../price/Price';
 
-const Skins = ({h1, key, src, p, p1}) => {
-    return(
+const Skins = ({ h1, key, src, p, p1 }) => {
+  return (
     <div className={styles.card} key={key}>
-    <img src={src} width={300} height={300}/>
-    <div className={styles.cardDescription}>
-      <h1>{h1}</h1>
-      <p>{p}</p>
-      <div className={styles.vbucks}>
-        <Price item={p1}/>
+      <img src={src} width={300} height={300} />
+      <div className={styles.cardDescription}>
+        <h1>{h1}</h1>
+        <p>{p}</p>
+        <div className={styles.vbucks}>
+          <Price item={p1} />
+        </div>
       </div>
     </div>
-  </div>
-    )
+  )
 }
 
 export default Skins;
