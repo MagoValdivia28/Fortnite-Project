e28269865be7049f0c678bb15c58dfb0ec57d828
felipe pedro <felipepedro.senai564@gmail.com>
2023-10-20 13:54:24 -0300

    inicio componente skin

--- app/components/pessoa/page.jsx
+++ app/components/pessoa/page.jsx
@@ -1,11 +1,15 @@
-const Pessoa = ({ imagem, nome, cargo, descricao }) => {
-    return(
-    <div>
-        <img src={imagem}/>
-        <h2>{nome}</h2>
-        <h3>{cargo}</h3>
-        <p>{descricao}</p>
-    </div>
-)};
+import styles from "./pessoa.module.css";
+const Pessoa = ({ imagem, nome, cargo, descricao, cor }) => {
+    return (
+        <div>
+            <div className={styles.cardPessoa} style={{ backgroundColor: cor}}>
+                <img src={imagem} height={200} width={250} />
+                <h2>{nome}</h2>
+                <h3>{cargo}</h3>
+                <p>{descricao}</p>
+            </div>
+        </div>
+    )
+};
 
 export default Pessoa;
