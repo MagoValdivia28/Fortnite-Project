f269bb319328cd113a9d10de483e47b8bf9d199d
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2023-10-20 16:32:47 -0300

    "link css"

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -1,24 +1,26 @@
 import styles from './footer.module.css';
-import Link from 'next/link';
+import {AiFillInstagram } from "react-icons/ai"
+import {AiFillGithub} from "react-icons/ai"
+
+
 const Footer = () => (
-    <div className={styles.main}>
-    <div className={styles.btn}>
-        <p  className={styles.item1}> copy </p>
+    <div>
+    <div className={styles.Article}>
+        <div className={styles.link}>
+          <a href="#">< AiFillInstagram /> </a>
+          <a href="#"><AiFillGithub /></a>
+        </div>
+
+        <div className={styles.copy}>
+            <p>Todos os direitos reservados &copy; 2023</p>
+
+
+        </div>
+
+      
+
     </div>
-
-    <div className={styles.footer_redesocial}>
-        <a href="#">instagram</a>
-
     </div>
-
-    <div className={styles.itens}>
-        <Link className={styles.item} href="/">home</Link>
-        <Link className={styles.item} href="/">skins</Link>
-        <Link className={styles.item} href="/">Maps</Link>
-        <Link className={styles.item} href="/sobrenos">equipe</Link>
-    </div>
-</div>
-
 )
 
 export default Footer;
