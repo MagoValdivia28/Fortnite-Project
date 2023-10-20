4b3511f8a0de09709c2bb47a0ecacbda309881ee
Pedro <pedrosouza.senai564@gmail.com>
2023-10-20 16:19:44 -0300

    teste

--- app/components/pessoa/page.jsx
+++ app/components/pessoa/page.jsx
@@ -1,15 +1,34 @@
 import styles from "./pessoa.module.css";
-const Pessoa = ({ imagem, nome, cargo, descricao, cor }) => {
-    return (
-        <div>
-            <div className={styles.cardPessoa} style={{ backgroundColor: cor}}>
-                <img src={imagem} height={200} width={250} />
-                <h2>{nome}</h2>
-                <h3>{cargo}</h3>
-                <p>{descricao}</p>
-            </div>
+const Pessoa = ({ imagem, nome, descricao }) => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.card}>
+        <img src={imagem} />
+        <div className={styles.cardContent}>
+          <h2>
+            {nome}
+          </h2>
+          <p>
+            {descricao}
+          </p>
         </div>
-    )
-};
+      </div>
+    </div>
+    {/* <div className={styles.cardPessoa}>
+            <img src={imagem} height={300} width={300} />
+            <div className={styles.info}>
+              <h2>{nome}</h2>
+              <h3>{cargo}</h3>
+            </div>
+            <div className={styles.descri}>
+              <p>{descricao}</p>
+            </div>
+          </div>
+          </div> */
+          * /}
+         
+  )
+  
+}
 
 export default Pessoa;
