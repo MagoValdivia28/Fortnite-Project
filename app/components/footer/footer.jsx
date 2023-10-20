78961f88af224bedc95d3501631d5d0186f90a3c
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2023-10-20 11:05:02 -0300

    "footer"

---
+++ app/components/footer/footer.jsx
@@ -0,0 +1,23 @@
+import styles from './footer.module.css';
+import Link from 'next/link';
+const Footer = () => (
+    <div className={styles.main}>
+    <div className={styles.btn}>
+        <p  className={styles.item1}> copy </p>
+    </div>
+
+    <div className={styles.footer_redesocial}>
+
+    </div>
+
+    <div className={styles.itens}>
+        <Link className={styles.item} href="/">home</Link>
+        <Link className={styles.item} href="/">skins</Link>
+        <Link className={styles.item} href="/">Maps</Link>
+        <Link className={styles.item} href="/sobrenos">equipe</Link>
+    </div>
+</div>
+
+)
+
+export default Footer;
