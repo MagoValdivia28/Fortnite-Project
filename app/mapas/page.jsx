d08ba5ba37ed79bd5cf08704954a9f2862c06c84
guilegal <guilhermeodorissio.564@gmail.com>
2023-10-23 17:03:35 -0300

    alteração

--- app/mapas/page.jsx
+++ app/mapas/page.jsx
@@ -1,6 +1,5 @@
 import Mapinfo from "../components/infomap/page";
-import styles from "./mapas.module.css"
-import Image from "next/image";
+import styles from "./page.module.css";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 
@@ -11,9 +10,12 @@ const Mapas = () => (
     <div className={styles.main}>
          <Header />
          <div className={styles.cards}>
-        <Mapas imagem={"/"}
+        <Mapinfo imagem={"/fortinite primeiro mapa.png"} nome={"Versao 1.6.0"} descrição={"O primeiro mapa do jogo"} cor={"#FF0000"}/>
+        <Mapinfo imagem={"/fortinite primeiro mapa.png"} nome={"Versao 1.6.0"} descrição={"O primeiro mapa do jogo"} cor={"#FF0000"}/>
          </div>
 
     </div>
     
 );
+
+export default Mapas;
