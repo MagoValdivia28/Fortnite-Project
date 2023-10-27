33d733bc7a1b36ebb2b14e7cba8d94210757fff5
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2023-10-27 14:36:12 -0300

    "Header e Footer"

--- app/noticia3/page.jsx
+++ app/noticia3/page.jsx
@@ -1,8 +1,13 @@
 import styles from "./page.module.css";
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
 const Pessoa = () => {
     return (
+
         <div>
-            <div>
+                    <Header/>
+                    <div>
+                    <div>
             <h1>REGRAS PARA CRIADORES DE ILHAS DO FORTNITE</h1>
             <p>            16.10.2023
 Equipe Fortnite
@@ -10,24 +15,53 @@ Equipe Fortnite
             </div>
 
             <div>
-            ATUALIZAÇÃO (16 de outubro de 2023)
-Fizemos diversas atualizações de políticas. As principais mudanças foram:
+          <h3> ATUALIZAÇÃO (16 de outubro de 2023)</h3> 
+          <p>
+          Fizemos diversas atualizações de políticas. As principais mudanças foram:
 
 Atualizamos a regra 1.15 (de acordo com a classificação etária de Fortnite): as imagens e textos usados para divulgar a sua experiência devem ser apropriados para o público geral (p. ex.: conteúdo que entraria na classificação ESRB 10+ ou menos). As imagens aprovadas anteriormente precisam seguir esta atualização.
 Adicionamos a regra 1.18 (Apresentação do Conteúdo da Ilha) 
 Tire um tempo para ler as regras atualizadas e se familiarizar com elas.
+          </p>
+
             </div>
             <div>
             <h1>BOAS-VINDAS, CRIADOR(A) DO FORTNITE! </h1>
-            As ferramentas Modo Criativo do Fortnite e Unreal Editor para Fortnite permitem que você crie Ilhas que podem ser publicadas no Fortnite.
+          <p>As ferramentas Modo Criativo do Fortnite e Unreal Editor para Fortnite permitem que você crie Ilhas que podem ser publicadas no Fortnite.
 
 Nosso intuito é que o Fortnite seja um lugar seguro e divertido para todos jogarem e se distraírem com os amigos, exatamente como dizemos nas Regras da Comunidade. 
 
 Ao criar novas Ilhas, jogos e experiências dentro do Fortnite, precisamos da sua ajuda para manter o jogo seguro, divertido e acolhedor para todos. Também queremos facilitar a criação e o sucesso da sua Ilha no Modo Criativo do Fortnite. 
 
-Essas regras descrevem as maneiras apropriadas para criar, promover e monetizar as suas Ilhas no Modo Criativo do Fortnite, incluindo algumas recomendações e restrições. Pedimos que você leia com atenção antes de publicar sua primeira Ilha do Fortnite. Boa sorte! 
+Essas regras descrevem as maneiras apropriadas para criar, promover e monetizar as suas Ilhas no Modo Criativo do Fortnite, incluindo algumas recomendações e restrições. Pedimos que você leia com atenção antes de publicar sua primeira Ilha do Fortnite. Boa sorte! </p>  
             </div>
 
+            <div>
+                <h1>RESUMINDO:</h1>
+                <p>Seja pelo Fortnite ou pelo Unreal Editor para Fortnite, toda Ilha que você cria é sua responsabilidade, e você deve obedecer as seguintes regras:
+
+Suas Ilhas devem estar de acordo tanto com as regras abaixo quanto com as Regras de Conteúdo da Epic Games.
+
+Seu comportamento deve estar de acordo tanto com as regras abaixo quanto com as Regras da Comunidade da Epic Games.
+
+Qualquer divulgação da sua Ilha, seja dentro ou fora do Fortnite, deve estar de acordo tanto com as regras abaixo quanto com as Diretrizes Sobre Gestão de Marca do Modo Criativo do Fortnite. </p>
+            </div>
+
+            <div>
+                <h1>REGRAS</h1>
+                <h3> REGRAS E DIRETRIZES DE CONTEÚDO</h3>
+                <p>À medida que trabalharmos juntos para garantir que o Fortnite continue sendo um ambiente seguro e acolhedor para todos, seu conteúdo do Fortnite precisa seguir as seguintes regras:
+
+<h5>1.1 Um Ambiente Seguro —</h5> Não é permitido criar conteúdo que envolva ou promova atividades ilegais, incluindo assédio, bullying, ataques, doxing (tornar público dados privados de uma pessoa), golpes, apostas e jogos de azar, drogas ilegais, crueldade com animais, fraude, hacking e pirataria. O Fortnite não é o lugar para conteúdo que incite ou faça apologia à violência contra outras pessoas.
+
+1.2. Não Compartilhe Informações Pessoais — Para respeitar a privacidade e ajudar a combater práticas enganosas (phishing, compra e venda de informações da conta), não compartilhe ou solicite informações pessoais na sua Ilha. Há uma exceção a esta regra: você pode incluir seus nomes/canais de redes sociais se eles não incluírem uma URL, código QR ou solicitação para mensagens privadas ou diretas. Para preservar a privacidade dos outros, você não pode juntar, compartilhar ou explorar informações pessoais de indivíduos nem organizações através da sua Ilha. Não inclua vídeos nem fotos de pessoas (nem você nem terceiros) na sua Ilha ou nos metadados dela. Os bate-papos de voz e texto da Epic no Fortnite contam com proteções como configurações de privacidade, controle dos pais e denúncias de jogadores. Por isso, não ofereça outras formas de comunicação na sua Ilha, como caixas de bate-papo ou teclados personalizados. 
+
+</p>
+            </div>
+
+
+                    </div>
+                    <Footer/>
 
         </div>
     )
