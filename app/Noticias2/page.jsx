e78d84c5bab15b07cb797900499fda2875064511
guilegal <guilhermeodorissio.564@gmail.com>
2023-10-27 11:12:32 -0300

    importação da header e do footer e estilização

--- app/Noticias2/page.jsx
+++ app/Noticias2/page.jsx
@@ -1,10 +1,12 @@
 import Footer from '../components/footer/footer'
+import Header from '../components/header/header'
 import styles from './page.module.css'
 import Image from 'next/image'
 
 const Noticia2 = () => {
     return (
         <>
+         <Header/>
         <div className={styles.main}>
             <img className={styles.img2}  src="	https://cdn2.unrealengine.com/discord-gfx-blogheader-1900x600-89c1c2c109a8.jpg"/>
 
@@ -20,3 +22,5 @@ const Noticia2 = () => {
     )
 
 }
+
+export default Noticia2
