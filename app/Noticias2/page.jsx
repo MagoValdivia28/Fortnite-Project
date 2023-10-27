d58a46e779c05acef7fb3d94ebee8dadb065cf4a
guilegal <guilhermeodorissio.564@gmail.com>
2023-10-27 14:49:22 -0300

    estilizando noticia 2

--- app/Noticias2/page.jsx
+++ app/Noticias2/page.jsx
@@ -1,23 +1,22 @@
 import Footer from '../components/footer/footer'
 import Header from '../components/header/header'
 import styles from './page.module.css'
-import Image from 'next/image'
+
 
 const Noticia2 = () => {
     return (
         <>
-         <Header/>
-        <div className={styles.main}>
-            <img className={styles.img2}  src="	https://cdn2.unrealengine.com/discord-gfx-blogheader-1900x600-89c1c2c109a8.jpg"/>
+            <Header />
+            <div className={styles.main}>
+                <img className={styles.img2} src="	https://cdn2.unrealengine.com/fncs-global-championship-2023-1900x600-73be5bea2d7e.jpg" />
+                <div>
+                    <h1><strong>CAMPEONATO GLOBAL DO FNCS 2023 — PROGRAMAÇÃO DAS TRANSMISSÕES E CAMPEONATO DA COMUNIDADE!</strong></h1>
+                    <span>24.10.2023</span>
+                    <p>E aí, pessoal! A Tabela de Liderança do Fortnite no Discord voltou trazendo recompensas no jogo para jogar com amigos do Discord. Além disso, ganhe um mês de Discord Nitro só de participar.</p>
+                </div>
 
-            <div>
-                <h1> 2</h1>
             </div>
-           
-            <p>Noticia  2</p>
-            
-        </div>
-        <Footer/>
+            <Footer />
         </>
     )
 
