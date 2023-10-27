ee24bdf8b0f40dfc939d3277716d301b045625d6
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2023-10-27 14:24:37 -0300

    "Noticia3"

---
+++ app/noticia3/page.jsx
@@ -0,0 +1,36 @@
+import styles from "./page.module.css";
+const Pessoa = () => {
+    return (
+        <div>
+            <div>
+            <h1>REGRAS PARA CRIADORES DE ILHAS DO FORTNITE</h1>
+            <p>            16.10.2023
+Equipe Fortnite
+Última Atualização: 16 de outubro de 2023</p>
+            </div>
+
+            <div>
+            ATUALIZAÇÃO (16 de outubro de 2023)
+Fizemos diversas atualizações de políticas. As principais mudanças foram:
+
+Atualizamos a regra 1.15 (de acordo com a classificação etária de Fortnite): as imagens e textos usados para divulgar a sua experiência devem ser apropriados para o público geral (p. ex.: conteúdo que entraria na classificação ESRB 10+ ou menos). As imagens aprovadas anteriormente precisam seguir esta atualização.
+Adicionamos a regra 1.18 (Apresentação do Conteúdo da Ilha) 
+Tire um tempo para ler as regras atualizadas e se familiarizar com elas.
+            </div>
+            <div>
+            <h1>BOAS-VINDAS, CRIADOR(A) DO FORTNITE! </h1>
+            As ferramentas Modo Criativo do Fortnite e Unreal Editor para Fortnite permitem que você crie Ilhas que podem ser publicadas no Fortnite.
+
+Nosso intuito é que o Fortnite seja um lugar seguro e divertido para todos jogarem e se distraírem com os amigos, exatamente como dizemos nas Regras da Comunidade. 
+
+Ao criar novas Ilhas, jogos e experiências dentro do Fortnite, precisamos da sua ajuda para manter o jogo seguro, divertido e acolhedor para todos. Também queremos facilitar a criação e o sucesso da sua Ilha no Modo Criativo do Fortnite. 
+
+Essas regras descrevem as maneiras apropriadas para criar, promover e monetizar as suas Ilhas no Modo Criativo do Fortnite, incluindo algumas recomendações e restrições. Pedimos que você leia com atenção antes de publicar sua primeira Ilha do Fortnite. Boa sorte! 
+            </div>
+
+
+        </div>
+    )
+};
+
+export default Pessoa;
