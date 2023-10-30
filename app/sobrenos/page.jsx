05653ea5f96007bed55d0812ae867caa2f5aa104
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2023-10-30 14:53:04 -0300

    "Sobre nós"

--- app/sobrenos/page.jsx
+++ app/sobrenos/page.jsx
@@ -6,31 +6,32 @@ import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 
 const Sobrenos = () => (
-    <>
+   
+   <div className={styles.fundo}>
         <Header />
         <div className={styles.main}>
             <div className={styles.card1}>
                 <Image className={styles.imgLogo} src={"/enigmaa.jpg"} width={256} height={200} />
                 <Image className={styles.imgEquipe} src={"/foto-equipe.jpeg"} width={256} height={200} />
                 <p>
-                    Em um mundo repleto de desafios e mistérios a serem desvendados, a equipe "ENIGMA" se destaca como um grupo unido e comprometido em superar obstáculos e desvendar os segredos do amanhã. Sob a liderança inspirada de Felipe Pedro, e com a participação ativa dos membros Pedro Isac, Guilherme Rocha, Thayna Vazzoler, Matheus Coco e Matheus Gomes, essa equipe é a personificação da colaboração, inovação e determinação.
+                    Em um mundo repleto de desafios e mistérios a serem desvendados, a equipe "ENIGMA" se destaca como um grupo unido e comprometido em superar obstáculos e desvendar os segredos do amanhã. Sob a liderança inspirada de Felipe Pedro, e com a participação ativa dos membros Pedro Isac, Guilherme Rocha, Thayná Vazzoler, Matheus Coco e Matheus Gomes, essa equipe é a personificação da colaboração, inovação e determinação.
                 </p>
             </div>
             <div className={styles.container1}>
                 <div className={styles.container2}>
-                    <Pessoa imagem={"/fe.jpeg"} nome={"Felipe Pedro"} cargo={"TechLead"} descricao={"2500 Vbucks"} />
-                    <Pessoa imagem={"/pe.jpg"} nome={"Pedro Isac"} cargo={"Dev"} descricao={"2000 Vbucks"} />
-                    <Pessoa imagem={"/rocha.jpeg"} nome={"Guilherme Rocha"} cargo={"Dev"} descricao={"2000 Vbucks"} />
+                    <Pessoa imagem={"/fe.jpeg"} nome={"Felipe Pedro"} descricao={"TechLead"} />
+                    <Pessoa imagem={"/pe.jpg"} nome={"Pedro Isac"} descricao={"Dev"} />
+                    <Pessoa imagem={"/rocha.jpeg"} nome={"Guilherme Rocha"} descricao={"2000 Vbucks"} />
                 </div>
                 <div className={styles.container2}>
-                    <Pessoa imagem={"/pravel.jpeg"} nome={"Matheus Gomes"} cargo={"Dev"} descricao={"2000 Vbucks"} />
-                    <Pessoa imagem={"/matheus coco.webp"} nome={"Matheus Côcco"} cargo={"Dev"} descricao={"2000 Vbucks"} />
-                    <Pessoa imagem={"/thayna.png"} nome={"Thayna Vazzoler"} cargo={"Dev"} descricao={"2000 Vbucks"} />
+                    <Pessoa imagem={"/pravel.jpeg"} nome={"Matheus Gomes"} descricao={"Dev"} />
+                    <Pessoa imagem={"/matheus coco.webp"} nome={"Matheus Côcco"} descricao={"Dev"} />
+                    <Pessoa imagem={"/thayna.png"} nome={"Thayna Vazzoler"} descricao={"Dev"} />
                 </div>
             </div>
         <Footer />
         </div>
-    </>
+  </div>
 );
 
 
