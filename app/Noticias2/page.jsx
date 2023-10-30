8fcfc5b82b071b87286f976c800be1283ab1e72e
guilegal <guilhermeodorissio.564@gmail.com>
2023-10-30 10:41:38 -0300

    alteração

--- app/Noticias2/page.jsx
+++ app/Noticias2/page.jsx
@@ -33,15 +33,42 @@ const Noticia2 = () => {
                     <li className={styles.li}>Assista por 1 hora no Dia 3 no Lar das Lendas ou em competitive.fortnite.com: <strong>Acessório para as Costas Escudo Eficiente</strong></li>
                     <p className={styles.texto}>Esses itens não estarão mais disponíveis para resgate por meio de saques do Fortnite após o fim da transmissão oficial do Campeonato Global do FNCS 2023, em 15 de outubro, mas, posteriormente, podem ser disponibilizados para compra na Loja de Itens do Fortnite.</p>
                     <h2 className={styles.tituloNoticia}>TRANSMISSÃO SEM COMENTÁRIOS</h2>
-                    <p className={styles.texto}>Caso queira organizar o seu próprio grupo de espectadores ou praticar as suas habilidades de comentarista enquanto ganha saques do Fortnite, disponibilizaremos uma transmissão sem comentários em fn.gg/cleanfeed. Não se esqueça de ler e aceitar os nossos Termos de Serviço antes de acessar a transmissão.</p>
+                    <p className={styles.texto}>Caso queira organizar o seu próprio grupo de espectadores ou praticar as suas habilidades de comentarista enquanto ganha saques do Fortnite, disponibilizaremos uma transmissão sem comentários em <a href={"https://competitive.fortnite.com/cleanfeed"}>fn.gg/cleanfeed</a>. Não se esqueça de ler e aceitar os nossos Termos de Serviço antes de acessar a transmissão.</p>
                     <p className={styles.texto}>A transmissão sem comentários nada mais é que uma transmissão apenas do jogo, sem comentários nem narração!</p>
                     <img className={styles.img} src="https://cdn2.unrealengine.com/fncs-global-championship-community-cup-1920x1080-5a9474af4cec.jpg" />
-                    <p className={styles.texto}>Deixe Aira e Max Maxximista ainda mais estilosos com outros itens da Loja:</p>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                    <p className={styles.center}>
+                        <em className={styles.emm}>Os Trajes Kyra Campeã e Infinito Cósmico voltarão para a Loja no futuro.</em></p>
+                    <p className={styles.texto}>Concederemos esses itens aos jogadores de maior pontuação em cada região de pareamento! Qual é o tipo do torneio? O Campeonato Global da Comunidade do FNCS é um evento de <strong> Battle Royale (Duplas).</strong></p>
+                    <p className={styles.texto}>Os competidores desse Campeonato poderão jogar até dez partidas em suas regiões num período de três horas. Os horários específicos de cada região podem ser encontrados na aba "Competir", no Fortnite. A pontuação funcionará da seguinte maneira:</p>
+                    <h4 className={styles.subtituloNoticia}>COLOCAÇÃO NA PARTIDA</h4>
+                    
+                        <li className={styles.lii}>Vitória Royale: 65 pontos</li>
+                        <li className={styles.lii}>2ª: 56 pontos</li>
+                        <li className={styles.lii}>3ª: 52 pontos</li>
+                        <li className={styles.lii}>4ª: 48 pontos</li>
+                        <li className={styles.lii}>5ª: 44 pontos</li>
+                        <li className={styles.lii}>6ª: 40 pontos</li>
+                        <li className={styles.lii}>7ª: 38 pontos</li>
+                        <li className={styles.lii}>8ª: 36 pontos</li>
+                        <li className={styles.lii}>9ª: 34 pontos</li>
+                        <li className={styles.lii}>10ª: 32 pontos</li>
+                        <li className={styles.lii}>11ª: 30 pontos</li>
+                        <li className={styles.lii}>12ª: 28 pontos</li>
+                        <li className={styles.lii}>13ª: 26 pontos</li>
+                        <li className={styles.lii}>14ª: 24 pontos</li>
+                        <li className={styles.lii}>15ª: 22 pontos</li>
+                        <li className={styles.lii}>16ª: 20 pontos</li>
+                        <li className={styles.lii}>17ª: 18 pontos</li>
+                        <li className={styles.lii}>18ª: 16 pontos</li>
+                        <li className={styles.lii}>19ª: 14 pontos</li>
+                        <li className={styles.lii}>20ª: 12 pontos</li>
+                        <li className={styles.lii}>21ª: 10 pontos</li>
+                        <li className={styles.lii}>22ª: 8 pontos</li>
+                        <li className={styles.lii}></li>
+                        <li className={styles.lii}></li>
+                        <li className={styles.lii}></li>
+                        <li className={styles.lii}></li>
+                        <li className={styles.lii}></li>
 
                 </div>
             </div>
