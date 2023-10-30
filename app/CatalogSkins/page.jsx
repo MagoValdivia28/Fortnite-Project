1e819418b35ae199e08e8d60fa0e85e5358676c7
pravel <123407140+ElMatheus@users.noreply.github.com>
2023-10-30 08:29:11 -0300

    fix: loader

--- app/CatalogSkins/page.jsx
+++ app/CatalogSkins/page.jsx
@@ -7,11 +7,7 @@ import styles from './catalogoSkins.module.css';
 import Skin from '../components/skin/Skin';
 import InfoCard from '../components/infoCard/InfoCard';
 import PopUpCadastro from '../components/PopUpCadastro/cadastro';
-<<<<<<< HEAD
 import { PuffLoader } from 'react-spinners';
-=======
-import { MoonLoader } from 'react-spinners'
->>>>>>> 2be0cacbdc3e5496093db1ee2c50cf7fc3f2472b
 
 function CatalogSkins() {
   const [apiData, setApiData] = useState(null);
